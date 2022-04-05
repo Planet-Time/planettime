bbadfb545ee48e334c97a626a4149ec6ed00bee0
DatGuy <mcfun001@126.com>
2022-04-05 22:38:40 +0800

    create from a WeChat mini program template

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-1goy7xw317314fd9","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-1goy7xw317314fd9 --n quickstartFunctions --r --project "C:\Dev\planettime" --report_first --report
