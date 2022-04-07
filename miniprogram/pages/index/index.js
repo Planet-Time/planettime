6f79419fec2c9ec626bf87ea5a384729cf4068a9
Lin Xin <widayn@prmlk.com>
2022-04-07 18:44:37 +0800

    Add three.js

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,23 @@
 // index.js
 // const app = getApp()
-const { envList } = require('../../envList.js');
+// const { envList } = require('../../envList.js');
+
+// Page({
+  
+// });
+
+import {createScopedThreejs} from 'threejs-miniprogram'
 
 Page({
-  
-});
+  onReady() {
+    wx.createSelectorQuery()
+      .select('#webgl')
+      .node()
+      .exec((res) => {
+        const canvas = res[0].node
+        // 创建一个与 canvas 绑定的 three.js
+        const THREE = createScopedThreejs(canvas)
+        // 传递并使用 THREE 变量
+      })
+  }
+})
